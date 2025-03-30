4e31f65a3de344596ad70d7f4df6d5344f3278fd
Suvansh Agrawal <s22ubbu@yahoo.com>
2025-03-30 21:18:51 +0530

    added logs

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ import logger from "./utils/logger.js";
 dotenv.config();
 const app = express();
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
-console.log(allowedOrigins);
 
 // Security middleware
 app.use(securityHeaders);
@@ -22,8 +21,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
     origin: function (origin, callback) {
+      console.log("origin", allowedOrigins);
+      console.log("incoming origin", origin);
       if (!origin || allowedOrigins.includes(origin)) {
-        console.log("incoming origin", origin);
         callback(null, true);
       } else {
         callback(new Error("Blocked by CORS"));
